7f1e2529bb81fd840487803e83cbd0efbddf4448
IgorNefedov228 <ignef2705@gmail.com>
2024-12-15 14:36:17 +0300

    Add files via upload

---
+++ project/my_site/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def page(request):
+    return render(request, 'main/page.html')
+
+def pages(request):
+    return render(request, 'main/pages.html')
